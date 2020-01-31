4f8790045c6957406f216fffebf8a53b021e1afe
leovcunha <lvcunha@gmail.com>
2020-01-31 17:34:05 -0300

    pull datas started

--- src/containers/photographers/index.js
+++ src/containers/photographers/index.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { getAllPhotographers } from "../../actions";
 
-const Photographers = () => <div>Photographers here</div>;
+const Photographers = ({ allPhotographers, doGetPhotographers, token }) => {
+    useEffect(() => {
+        if (!allPhotographers.photographers) {
+            doGetPhotographers(token);
+        }
+    }, []);
 
-export default Photographers;
+    return ({allPhotographers.photographers && <div>{allPhotographers.photographers}</div>});
+};
+
+const mapStateToProps = state => ({
+    allPhotographers: state.allPhotographers,
+    token: state.auth.user.token
+});
+const mapDispatchToProps = dispatch => ({
+    doGetPhotographers: token => dispatch(getAllPhotographers(token))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Photographers));
